813c9a1d87a521de6c7b952466b9b34062bbbd39
LinuxServer <Jerome.Wolff1207@gmail.com>
2020-06-21 02:39:38 +0200

    Improve code coverage

---
+++ src/main/java/de/bluebox/wolff/candle/concurrent/future/AbstractSettableFuture.java
@@ -0,0 +1,112 @@
+package de.bluebox.wolff.candle.concurrent.future;
+
+import de.bluebox.wolff.candle.annotation.Nullable;
+import de.bluebox.wolff.candle.functional.Completable;
+import java.util.Objects;
+import java.util.StringJoiner;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.TimeoutException;
+import javax.annotation.concurrent.ThreadSafe;
+
+@ThreadSafe
+public abstract class AbstractSettableFuture<V> implements RunnableFuture<V>, Completable {
+  private V value;
+  private Throwable throwable;
+  private volatile boolean done;
+
+  protected AbstractSettableFuture() {}
+
+  @Override
+  public void run() {
+    this.setValue(null);
+  }
+
+  @Override
+  public boolean cancel(boolean mayInterruptIfRunning) {
+    return false;
+  }
+
+  @Override
+  public boolean isCancelled() {
+    return false;
+  }
+
+  @Override
+  public boolean isDone() {
+    return this.done;
+  }
+
+  @Override
+  public void complete() {
+    this.done = true;
+    this.notifyAll();
+  }
+
+  public synchronized void setValue(V value) {
+    this.value = value;
+    this.complete();
+  }
+
+  public synchronized void setException(Throwable throwable) {
+    this.throwable = throwable;
+    this.complete();
+  }
+
+  protected synchronized void waitForCompletion() throws InterruptedException {
+    while (!this.isDone()) {
+      this.wait();
+    }
+  }
+
+  protected synchronized void waitForCompletion(long timeout)
+      throws InterruptedException, TimeoutException {
+    long waitUntil = System.currentTimeMillis() + timeout;
+
+    while (!this.isDone()) {
+      this.wait(timeout);
+      timeout = waitUntil - System.currentTimeMillis();
+
+      if (timeout < 0) {
+        throw new TimeoutException();
+      }
+    }
+  }
+
+  @Nullable
+  protected final V value() {
+    return value;
+  }
+
+  @Nullable
+  protected final Throwable throwable() {
+    return throwable;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (this == object) {
+      return true;
+    }
+    if (object == null || getClass() != object.getClass()) {
+      return false;
+    }
+    AbstractSettableFuture<?> that = (AbstractSettableFuture<?>) object;
+    return this.done == that.done
+        && Objects.equals(this.value, that.value)
+        && Objects.equals(this.throwable, that.throwable);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.value, this.throwable, this.done);
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", AbstractSettableFuture.class.getSimpleName() + "[", "]")
+        .add("value=" + this.value)
+        .add("throwable=" + this.throwable)
+        .add("done=" + this.done)
+        .toString();
+  }
+}
