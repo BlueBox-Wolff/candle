4c89696b4ab44edd13d44dd1ca45415c16fc5959
LinuxServer <jerome.wolff1207@gmail.com>
2020-07-08 22:31:19 +0200

    Add documentations for file utilities

--- src/main/java/de/bluebox/wolff/candle/io/file/Files.java
+++ src/main/java/de/bluebox/wolff/candle/io/file/Files.java
@@ -7,17 +7,65 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 
+/**
+ * Collections of file utilities
+ *
+ * @author Jerome Wolff
+ * @since 1.0.0
+ */
 public final class Files {
   private Files() {}
 
+  /**
+   * <p>
+   *   Creates a file or folder that is deleted after the program ends.
+   * </p>
+   *
+   * <pre>
+   *   Files.createTemporaryFile("/home/", "test_file")
+   * </pre>
+   *
+   * @param path Path of the file
+   * @param fileName Name of the file
+   * @return the file that was created. If this file already exists, it will be returned.
+   * @throws IOException If the file or directory could not created
+   */
   public static File createTemporaryFile(String path, String fileName) throws IOException {
     return createTemporaryFile(Paths.get(path), fileName);
   }
 
+  /**
+   * <p>
+   *   Creates a file or folder that is deleted after the program ends.
+   * </p>
+   *
+   * <pre>
+   *   Files.createTemporaryFile(java.nio.file.Paths.get("/home/"), "test_file")
+   * </pre>
+   *
+   * @param path Path of the file
+   * @param fileName Name of the file
+   * @return the file that was created. If this file already exists, it will be returned.
+   * @throws IOException If the file or directory could not created
+   */
   public static File createTemporaryFile(Path path, String fileName) throws IOException {
     return createTemporaryFile(path.toFile(), fileName);
   }
 
+  /**
+   * <p>
+   *   Creates a file or folder that is deleted after the program ends.
+   * </p>
+   *
+   * <pre>
+   *   Files.createTemporaryFile(new java.io.File("/home/"), "test_file")
+   * </pre>
+   *
+   * @param path Path of the file
+   * @param fileName Name of the file
+   * @return the file that was created. If this file already exists, it will be returned.
+   * @throws IOException If the file or directory could not created
+   */
   public static File createTemporaryFile(File path, String fileName) throws IOException {
     File file = new File(path, fileName);
 
@@ -37,18 +85,68 @@ public final class Files {
     return file;
   }
 
+  /**
+   * <p>
+   *   Creates a new file or folder.
+   *   This method is only used to avoid code duplications.
+   * </p>
+   *
+   * <pre>
+   *   Files.createFileOrDirectory(new java.io.File("/home/test_file.xml"))
+   * </pre>
+   *
+   * @param file File or directory which should be created
+   * @return {@code true} if the file/folder could be created. Otherwise {@code false}
+   * @throws IOException If the file or directory could not created
+   */
   public static boolean createFileOrDirectory(File file) throws IOException {
     return file.isDirectory() ? file.mkdir() : file.createNewFile();
   }
 
-  public static boolean deleteDirectories(File[] directory) {
-    return deleteDirectories(Arrays.asList(directory));
+  /**
+   * <p>
+   *   Delete multiple directories at the same time
+   * </p>
+   *
+   * <pre>
+   *   Files.deleteDirectories(new File[] {new java.io.File("/home/"), new java.io.File("/home2/")})
+   * </pre>
+   *
+   * @param directories the folders to be deleted as array
+   * @return {@code true} if every folder could be deleted. Otherwise {@code false}
+   */
+  public static boolean deleteDirectories(File[] directories) {
+    return deleteDirectories(Arrays.asList(directories));
   }
 
+  /**
+   * <p>
+   *   Delete multiple directories at the same time
+   * </p>
+   *
+   * <pre>
+   *   Files.deleteDirectories(java.util.Arrays.asList(new File[] {new java.io.File("/home/"), new java.io.File("/home2/")}))
+   * </pre>
+   *
+   * @param directories the folders to be deleted as collection
+   * @return {@code true} if every folder could be deleted. Otherwise {@code false}
+   */
   public static boolean deleteDirectories(Collection<File> directories) {
     return directories.stream().map(Files::deleteDirectory).reduce(true, (a, b) -> a && b);
   }
 
+  /**
+   * <p>
+   *   Deletes the specified file only if it is a folder.
+   * </p>
+   *
+   * <pre>
+   *   Files.deleteDirectory(new java.io.File("/home/"))
+   * </pre>
+   *
+   * @param directory the folder to be deleted
+   * @return {@code true} if every folder could be deleted. Otherwise {@code false}
+   */
   public static boolean deleteDirectory(File directory) {
     return directory.isDirectory() && directory.delete();
   }
